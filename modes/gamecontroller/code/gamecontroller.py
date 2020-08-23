0d028ccd2d6ad9f259d8804f2f688570fadf049e
mythmaker9 <mythmaker9@gmail.com>
2020-08-23 15:35:56 -0400

    Game selector updates

--- modes/gamecontroller/code/gamecontroller.py
+++ modes/gamecontroller/code/gamecontroller.py
@@ -1,44 +1,84 @@
 from mpf.core.mode import Mode
 
 class Base(Mode):
+    
+
     def mode_init(self):
         print("Gamecontroller init...")
+        
 
     def mode_start(self, **kwargs):
         # The mode_start method needs **kwargs because some events that
         # start modes pass additional parameters
 
         self.add_mode_event_handler('player_add_request', self.gc_checkaddplayer)
-        self.add_mode_event_handler('mode_selected', self.gc_modeselected)
+
+
+        self.add_mode_event_handler('gameselect_Easy_selected', self.gc_modeeasyselected)
+        self.add_mode_event_handler('gameselect_Normal_selected', self.gc_modenormalselected)
+        self.add_mode_event_handler('gameselect_Hardcore_selected', self.gc_modehardselected)
+        self.add_mode_event_handler('gameselect_Competition_selected', self.gc_modecompetitionselected)
+        self.add_mode_event_handler('gameselect_COOP_selected', self.gc_modecoopselected)
+        self.add_mode_event_handler('gameselect_Story_selected', self.gc_modestoryselected)
+
+
+        #self.add_mode_event_handler('mode_selected', self.gc_modeselected)
         
         self.add_mode_event_handler('player_added', self.gc_playeradded)
 
-    #MODE SELECTED - Mode is Selected, Set Required Functionality
-    def gc_modeselected(self, **kwargs):
-        if self.player.game_mode=="STORY":
-            #Need to turn on endless ball save as well
-            self.machine.game.balls_per_game==1
-        else:
-            self.machine.game.balls_per_game==3
+    #MODE IS SELECTED
+    def gc_modeeasyselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "EASY")
+        self.machine.variables.set_machine_var("game_mode_display", "Easy Mode")
+        self.machine.game.balls_per_game==3
+    def gc_modenormalselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "NORMAL")
+        self.machine.variables.set_machine_var("game_mode_display", "Normal Mode")
+        self.machine.game.balls_per_game==3
+    def gc_modehardselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "HARDCORE")
+        self.machine.variables.set_machine_var("game_mode_display", "Hardcore Mode")
+        self.machine.game.balls_per_game==3
+    def gc_modecompetitionselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "COMPETITION")
+        self.machine.variables.set_machine_var("game_mode_display", "Competition Mode")
+        self.machine.game.balls_per_game==3
+    def gc_modecoopselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "COOP")
+        self.machine.variables.set_machine_var("game_mode_display", "Cooperative Mode")
+        self.machine.game.balls_per_game==3
+    def gc_modestoryselected(self, **kwargs):
+        self.machine.variables.set_machine_var("game_mode", "STORY")
+        self.machine.variables.set_machine_var("game_mode_display", "Story Mode")
+        self.machine.game.balls_per_game==1
 
     #PLAYER ADDING - Do not allow adding of player if game select screen is active, or certain critera applies
     def gc_checkaddplayer(self, **kwargs):
-        if self.machine.mode_controller.is_active("gameselect"):
+        if self.machine.mode_controller.is_active("gameselect"):        #Do not allow adding player if selecting screen
             return (False)
-        elif self.player.game_mode=="STORY":
-            return (False)
+        elif self.player.game_mode=="EASY":
+            return (True)
+        elif self.player.game_mode=="NORMAL":
+            return (True)
+        elif self.player.game_mode=="HARDCORE":
+            return (True)
+        elif self.player.game_mode=="COMPETITION":
+            return (True)
         elif self.player.game_mode=="COOP":
             #DO DIFFERENT THINGS HERE
             if self.machine.game.balls_per_game < 12:
                 self.machine.game.balls_per_game+=3
             return (False)
-        else:
+        elif self.player.game_mode=="STORY":
+            return (False)
+        else: #ERROR
             return (True)
 
     #PLAYER ADDED - When a new player is added, make sure mode is applied to their profile.
     def gc_playeradded(self, **kwargs):
         # Set Game Mode for Newly Added Players
-        #kwargs['player'].game_mode = self.player[1].game_mode
+        player = kwargs['player']
+        player.game_mode = self.machine.variables.get_machine_var("game_mode")
         return (True)
 
     def mode_stop(self, **kwargs):
@@ -46,3 +86,4 @@ class Base(Mode):
         # stop modes pass additional parameters
 
         print("Game Controller Stopping")
+
